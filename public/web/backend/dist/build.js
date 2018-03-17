e74f2b3ed1659545a0698d01f50e6488d8c960ad
janber <janber_zhang@163.com>
2018-03-17 21:26:28 +0800

    test

--- public/web/backend/dist/build.js
+++ public/web/backend/dist/build.js
@@ -50281,9 +50281,31 @@
 	});
 	// <template>
 	//   <div class="basic_system app-warp">
-	//     <Card style="width:800px">
+	//     <Card style="width:100%">
 	//       <p slot="title">邮件模版</p>
-	//
+	//       <table class="list-table">
+	//         <thead>
+	//           <tr>
+	//             <th width="100">编号</th>
+	//             <th width="300">代码</th>
+	//             <th width="200">名称</th>
+	//             <th width="200">通知对象</th>
+	//             <th width="200">模版内容</th>
+	//           </tr>
+	//         </thead>
+	//         <tbody>
+	//           <tr v-for="(item, index) in data_list" :key="item.code">
+	//             <td>{{index}}</td>
+	//             <td>{{item.code}}</td>
+	//             <td>{{item.name}}</td>
+	//             <td>{{item.target}}</td>
+	//             <td>
+	//               <span class="handle" @click="showDetail">编辑</span>
+	//               <span class="handle" @click="showDetail">查看</span>
+	//             </td>
+	//           </tr>
+	//         </tbody>
+	//       </table>
 	//     </Card>
 	//   </div>
 	// </template>
@@ -50294,21 +50316,17 @@
 	  ready: function ready() {},
 	  data: function data() {
 	    return {
-	      data: {
-	        smtp_addr: 'smtp.exmail.qq.com',
-	        email_addr: 'info@andaobo.com',
-	        email_user: 'info@andaobo.com',
-	        email_pwd: 'A123456a',
-	        manager_email: 'info@andaobo.com',
-	        smtp_port: '25'
-	      },
+	      data_list: [{ code: 'REGISTER_SUCCESS_NOTIFY_TO_USER', name: '注册成功', target: '客户' }, { code: 'REGISTER_SUCCESS_NOTIFY_TO_MANAGER', name: '用户注册', target: '管理员' }, { code: 'APPLY_VERIFY_NOTIFY_TO_MANAGER', name: '提交认证', target: '管理员' }, { code: 'VERIFY_SUCCESS_TO_USER', name: '认证成功', target: '客户' }, { code: 'VERIFY_FAIL_TO_USER', name: '认证失败', target: '客户' }, { code: 'APPLY_WITHDRAW_NOTIFY_TO_MANAGER', name: '申请出金', target: '管理员' }, { code: 'WITHDRAW_CHECK_SUCCESS_NOTIFY_TO_USER', name: '出金审核成功', target: '客户' }, { code: 'WITHDRAW_CHECK_FAIL_NOTIFY_TO_USER', name: '出金审核失败', target: '客户' }, { code: 'WITHDRAW_TRANSACTION_SUCCESS_NOTIFY_TO_USER', name: '出金转账通知', target: '客户' }, { code: 'DEPOSIT_SUCCESS_TO_MANAGER', name: '入金成功通知', target: '管理员' }],
 	      readonly: true
 	    };
 	  },
 
 	  methods: {
-	    switchModel: function switchModel(type) {
-	      this.readonly = type;
+	    showDetail: function showDetail() {
+	      this.$Modal.warning({
+	        title: '温馨提示',
+	        content: '<p>暂未开放该模块</p>'
+	      });
 	    }
 	  },
 	  components: {},
@@ -50321,7 +50339,7 @@
 /* 44 */
 /***/ function(module, exports) {
 
-	module.exports = "\n<div class=\"basic_system app-warp\">\n  <Card style=\"width:800px\">\n    <p slot=\"title\">邮件模版</p>\n          \n  </Card>\n</div>\n";
+	module.exports = "\n<div class=\"basic_system app-warp\">\n  <Card style=\"width:100%\">\n    <p slot=\"title\">邮件模版</p>\n    <table class=\"list-table\">\n      <thead>\n        <tr>\n          <th width=\"100\">编号</th>\n          <th width=\"300\">代码</th>\n          <th width=\"200\">名称</th>\n          <th width=\"200\">通知对象</th>\n          <th width=\"200\">模版内容</th>\n        </tr>\n      </thead>\n      <tbody>\n        <tr v-for=\"(item, index) in data_list\" :key=\"item.code\">\n          <td>{{index}}</td>\n          <td>{{item.code}}</td>\n          <td>{{item.name}}</td>\n          <td>{{item.target}}</td>\n          <td>\n            <span class=\"handle\" @click=\"showDetail\">编辑</span>\n            <span class=\"handle\" @click=\"showDetail\">查看</span>\n          </td>\n        </tr>\n      </tbody>\n    </table>\n  </Card>\n</div>\n";
 
 /***/ },
 /* 45 */,
--- public/web/backend/src/views/pages/basic/email_template.vue
+++ public/web/backend/src/views/pages/basic/email_template.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="basic_system app-warp">
-    <Card style="width:800px">
+    <Card style="width:100%">
       <p slot="title">邮件模版</p>
-            
+      <table class="list-table">
+        <thead>
+          <tr>
+            <th width="100">编号</th>
+            <th width="300">代码</th>
+            <th width="200">名称</th>
+            <th width="200">通知对象</th>
+            <th width="200">模版内容</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in data_list" :key="item.code">
+            <td>{{index}}</td>
+            <td>{{item.code}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.target}}</td>
+            <td>
+              <span class="handle" @click="showDetail">编辑</span>
+              <span class="handle" @click="showDetail">查看</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </Card>
   </div>
 </template>
@@ -17,21 +39,29 @@ export default {
   },
   data(){
     return {
-      data: {
-        smtp_addr: 'smtp.exmail.qq.com',
-        email_addr: 'info@andaobo.com',
-        email_user: 'info@andaobo.com',
-        email_pwd: 'A123456a',
-        manager_email: 'info@andaobo.com',
-        smtp_port: '25'
-      },
+      data_list: [
+        {code: 'REGISTER_SUCCESS_NOTIFY_TO_USER',name: '注册成功',target: '客户'},
+        {code: 'REGISTER_SUCCESS_NOTIFY_TO_MANAGER',name: '用户注册',target: '管理员'},
+        {code: 'APPLY_VERIFY_NOTIFY_TO_MANAGER',name: '提交认证',target: '管理员'},
+        {code: 'VERIFY_SUCCESS_TO_USER',name: '认证成功',target: '客户'},
+        {code: 'VERIFY_FAIL_TO_USER',name: '认证失败',target: '客户'},
+        {code: 'APPLY_WITHDRAW_NOTIFY_TO_MANAGER',name: '申请出金',target: '管理员'},
+        {code: 'WITHDRAW_CHECK_SUCCESS_NOTIFY_TO_USER',name: '出金审核成功',target: '客户'},
+        {code: 'WITHDRAW_CHECK_FAIL_NOTIFY_TO_USER',name: '出金审核失败',target: '客户'},
+        {code: 'WITHDRAW_TRANSACTION_SUCCESS_NOTIFY_TO_USER',name: '出金转账通知',target: '客户'},
+        {code: 'DEPOSIT_SUCCESS_TO_MANAGER',name: '入金成功通知',target: '管理员'}
+      ],
       readonly: true
     }
   },
   methods:{
-    switchModel(type){
-      this.readonly = type;
-    }
+    showDetail(){
+      this.$Modal.warning({
+        title: '温馨提示',
+        content: '<p>暂未开放该模块</p>'
+      });
+    },
+
   },
   components:{
 
