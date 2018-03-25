2772cdcdc73a10cd89fcb18e4511847c2a6602c8
janber <janber_zhang@163.com>
2018-03-25 14:16:48 +0800

    test

--- public/web/backend/dist/build.js
+++ public/web/backend/dist/build.js
@@ -52386,7 +52386,11 @@
 	//   <div class="mt4_init app-warp">
 	//     <Card style="width:600px">
 	//       <p slot="title">初始化</p>
-	//
+	//       <h1 class="mt4_init-title">注意事项</h1>
+	//       <p class="mt4_init-content">1.第一次初始化会将CRM系统里面的历史数据全部清空，请仅仅在系统第一次使用的时候进行操作！！！</p>
+	//       <p class="mt4_init-content">2.初始化完毕以后，初始化开关将被置为1,就不能在进行初始化。如果要再进行初始化，需要在MT4基本设置中将标志位修改为0。</p>
+	//       <p class="mt4_init-content">3.初始化的作用主要是将MT4中的数据包括，组，符号，用户，历史订单，同步到CRM系统中！！！</p>
+	//       <Button icon="nuclear" @click="init">初始化</Button>
 	//     </Card>
 	//   </div>
 	// </template>
@@ -52410,8 +52414,19 @@
 	  },
 
 	  methods: {
-	    switchModel: function switchModel(type) {
-	      this.readonly = type;
+	    init: function init() {
+	      var _this = this;
+
+	      this.$Modal.confirm({
+	        title: '警告',
+	        content: '<p>此次操作无法撤回，是否继续？</p>',
+	        onOk: function onOk() {
+	          _this.$Message.info('Clicked ok');
+	        },
+	        onCancel: function onCancel() {
+	          _this.$Message.info('Clicked cancel');
+	        }
+	      });
 	    }
 	  },
 	  components: {},
@@ -52424,7 +52439,7 @@
 /* 112 */
 /***/ function(module, exports) {
 
-	module.exports = "\n<div class=\"mt4_init app-warp\">\n  <Card style=\"width:600px\">\n    <p slot=\"title\">初始化</p>\n    \n  </Card>\n</div>\n";
+	module.exports = "\n<div class=\"mt4_init app-warp\">\n  <Card style=\"width:600px\">\n    <p slot=\"title\">初始化</p>\n    <h1 class=\"mt4_init-title\">注意事项</h1>\n    <p class=\"mt4_init-content\">1.第一次初始化会将CRM系统里面的历史数据全部清空，请仅仅在系统第一次使用的时候进行操作！！！</p>\n    <p class=\"mt4_init-content\">2.初始化完毕以后，初始化开关将被置为1,就不能在进行初始化。如果要再进行初始化，需要在MT4基本设置中将标志位修改为0。</p>\n    <p class=\"mt4_init-content\">3.初始化的作用主要是将MT4中的数据包括，组，符号，用户，历史订单，同步到CRM系统中！！！</p>\n    <Button icon=\"nuclear\" @click=\"init\">初始化</Button>\n  </Card>\n</div>\n";
 
 /***/ }
 /******/ ]);
--- public/web/backend/src/views/pages/mt4/init.vue
+++ public/web/backend/src/views/pages/mt4/init.vue
@@ -2,7 +2,11 @@
   <div class="mt4_init app-warp">
     <Card style="width:600px">
       <p slot="title">初始化</p>
-      
+      <h1 class="mt4_init-title">注意事项</h1>
+      <p class="mt4_init-content">1.第一次初始化会将CRM系统里面的历史数据全部清空，请仅仅在系统第一次使用的时候进行操作！！！</p>
+      <p class="mt4_init-content">2.初始化完毕以后，初始化开关将被置为1,就不能在进行初始化。如果要再进行初始化，需要在MT4基本设置中将标志位修改为0。</p>
+      <p class="mt4_init-content">3.初始化的作用主要是将MT4中的数据包括，组，符号，用户，历史订单，同步到CRM系统中！！！</p>
+      <Button icon="nuclear" @click="init">初始化</Button>
     </Card>
   </div>
 </template>
@@ -29,8 +33,17 @@ export default {
     }
   },
   methods:{
-    switchModel(type){
-      this.readonly = type;
+    init(){
+      this.$Modal.confirm({
+          title: '警告',
+          content: '<p>此次操作无法撤回，是否继续？</p>',
+          onOk: () => {
+              this.$Message.info('Clicked ok');
+          },
+          onCancel: () => {
+              this.$Message.info('Clicked cancel');
+          }
+      });
     }
   },
   components:{
